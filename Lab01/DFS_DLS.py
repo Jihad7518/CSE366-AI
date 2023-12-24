2bc29539608a202280dceceb9028a2da50b9f7a7
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-24 23:37:34 +0600

    Update DFS_DLS.py

--- Lab01/DFS_DLS.py
+++ Lab01/DFS_DLS.py
@@ -22,3 +22,43 @@ graph = {
   '20' : []
 
 }
+
+visited = set() # Set to keep track of visited nodes of graph.
+
+def dfs(visited, graph, node):  #function for dfs
+    if node not in visited:
+        print (node)
+        visited.add(node)
+        for neighbour in graph[node]:
+            dfs(visited, graph, neighbour)
+
+# Driver Code
+print("Following is the Depth-First Search")
+dfs(visited, graph, '0')
+
+#DLS
+
+# Define a graph as an adjacency list
+graph = {
+  '0' : ['1','2','3'],
+  '1' : ['4','5','6'],
+  '2' : ['7'],
+  '3' : ['8','9'],
+  '4' : ['10','11'],
+  '5' : ['12'],
+  '6' : ['13','14'],
+  '7' : ['15', '16'],
+  '8' : ['17'],
+  '9' : ['18','19','20'],
+  '10' : [],
+  '11' : [],
+  '12' : [],
+  '13' : [],
+  '14' : [],
+  '15' : [],
+  '16' : [],
+  '17' : [],
+  '18' : [],
+  '19' : [],
+  '20' : []
+}
