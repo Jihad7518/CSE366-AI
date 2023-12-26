bf5cc0b7c93cc8e9be636b3d6c8e08d38bc27174
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-26 08:31:16 +0600

    Update Searching_With_Huristics.py

--- Lab02/Searching_With_Huristics.py
+++ Lab02/Searching_With_Huristics.py
@@ -25,3 +25,32 @@ class Graph:
 
         return H[n]
 
+   def a_star_algorithm(self, start_node, stop_node):
+        # open_list is a list of nodes which have been visited, but who's neighbors
+        # haven't all been inspected, starts off with the start node
+        # closed_list is a list of nodes which have been visited
+        # and who's neighbors have been inspected
+        open_list = set([start_node])
+        closed_list = set([])
+
+        # g contains current distances from start_node to all other nodes
+        # the default value (if it's not found in the map) is +infinity
+        g = {}
+
+        g[start_node] = 0
+
+        # parents contains an adjacency map of all nodes
+        parents = {}
+        parents[start_node] = start_node
+
+        while len(open_list) > 0:
+            n = None
+
+            # find a node with the lowest value of f() - evaluation function
+            for v in open_list:
+                if n == None or g[v] + self.h(v) < g[n] + self.h(n):
+                    n = v;
+
+            if n == None:
+                print('Path does not exist!')
+                return None
