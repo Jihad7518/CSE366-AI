d0c25a8bd60633dce506c3213984c3336a79612d
Md. Jihad <125210346+Jihad7518@users.noreply.github.com>
2023-12-26 08:32:00 +0600

    Update Searching_With_Huristics.py

--- Lab02/Searching_With_Huristics.py
+++ Lab02/Searching_With_Huristics.py
@@ -120,3 +120,38 @@ def aStarAlgo(start_node, stop_node):
         #start_node is root node i.e it has no parent nodes
         #so start_node is set to its own parent node
         parents[start_node] = start_node
+
+        while len(open_set) > 0:
+            n = None
+
+            #node with lowest f() is found
+            for v in open_set:
+                if n == None or g[v] + heuristic(v) < g[n] + heuristic(n):
+                    n = v
+
+
+            if n == stop_node or Graph_nodes[n] == None:
+                pass
+            else:
+                for (m, weight) in get_neighbors(n):
+                    #nodes 'm' not in first and last set are added to first
+                    #n is set its parent
+                    if m not in open_set and m not in closed_set:
+                        open_set.add(m)
+                        parents[m] = n
+                        g[m] = g[n] + weight
+
+
+                    #for each node m,compare its distance from start i.e g(m) to the
+                    #from start through n node
+                    else:
+                        if g[m] > g[n] + weight:
+                            #update g(m)
+                            g[m] = g[n] + weight
+                            #change parent of m to n
+                            parents[m] = n
+
+                            #if m in closed set,remove and add to open
+                            if m in closed_set:
+                                closed_set.remove(m)
+                                open_set.add(m)
